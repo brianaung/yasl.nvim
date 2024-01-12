19859b5d878ce6df7e5d6b23d8bc981f41e2c059
Brian Aung <brianaung16@gmail.com>
2024-01-13 03:47:46 +1100

    provide default groups

--- lua/yasl/init.lua
+++ lua/yasl/init.lua
@@ -30,14 +30,35 @@ end
 
 M.setup = function(opts)
 	vim.api.nvim_set_option("laststatus", 3)
-	local status_string = string.format("%s%s %s %s %s %s%s",
-		get_statusgroup(opts.sections.a, "a"),
-		get_statusgroup(opts.sections.b, "b"),
-		"%=",
-		get_statusgroup(opts.sections.c, "c"),
-		"%=",
-		get_statusgroup(opts.sections.d, "d"),
-		get_statusgroup(opts.sections.e, "e"))
+
+	local default_groups = {
+		["a"] = { components = { "mode" } },
+		["b"] = { components = { "diagnostics" } },
+		["c"] = { components = { "filename", "branch" } },
+		["d"] = { components = { "filetype" } },
+		["e"] = { components = { "location", "progress" } },
+	}
+
+	local status_string = ""
+	if opts.sections ~= nil then
+		status_string = string.format("%s%s %s %s %s %s%s",
+			get_statusgroup(opts.sections.a, "a"),
+			get_statusgroup(opts.sections.b, "b"),
+			"%=",
+			get_statusgroup(opts.sections.c, "c"),
+			"%=",
+			get_statusgroup(opts.sections.d, "d"),
+			get_statusgroup(opts.sections.e, "e"))
+	else
+		status_string = string.format("%s%s %s %s %s %s%s",
+			get_statusgroup(default_groups["a"], "a"),
+			get_statusgroup(default_groups["b"], "b"),
+			"%=",
+			get_statusgroup(default_groups["c"], "c"),
+			"%=",
+			get_statusgroup(default_groups["d"], "d"),
+			get_statusgroup(default_groups["e"], "e"))
+	end
 	vim.api.nvim_set_option("statusline", status_string)
 end
 
