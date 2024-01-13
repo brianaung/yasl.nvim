5bc00ec9cb133ccfbf17fa70e1293a9b712dc81c
Brian Aung <brianaung16@gmail.com>
2024-01-14 03:46:21 +1100

    chore: lets move these

--- lua/yasl/component.lua
+++ lua/yasl/component.lua
@@ -1,5 +1,38 @@
 local M = {}
 
+local function split_lines(str)
+	local lines = {}
+	for s in str:gmatch("[^\r\n]+") do
+		table.insert(lines, s)
+	end
+	return lines
+end
+
+-- Adapted from https://github.com/nvim-lualine/lualine.nvim
+local function process_diff(data)
+	local added, removed, modified = 0, 0, 0
+	for _, line in ipairs(data) do
+		if string.find(line, [[^@@ ]]) then
+			local tokens = vim.fn.matchlist(line, [[^@@ -\v(\d+),?(\d*) \+(\d+),?(\d*)]])
+			local line_stats = {
+				mod_count = tokens[3] == nil and 0 or tokens[3] == '' and 1 or tonumber(tokens[3]),
+				new_count = tokens[5] == nil and 0 or tokens[5] == '' and 1 or tonumber(tokens[5]),
+			}
+			if line_stats.mod_count == 0 and line_stats.new_count > 0 then
+				added = added + line_stats.new_count
+			elseif line_stats.mod_count > 0 and line_stats.new_count == 0 then
+				removed = removed + line_stats.mod_count
+			else
+				local min = math.min(line_stats.mod_count, line_stats.new_count)
+				modified = modified + min
+				added = added + line_stats.new_count - min
+				removed = removed + line_stats.mod_count - min
+			end
+		end
+	end
+	return { added = added, modified = modified, removed = removed }
+end
+
 function M.branch()
 	local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
 	if branch ~= "" then
@@ -23,51 +56,14 @@ function M.diagnostics()
 end
 
 function M.gitdiff()
-	-- Adapted from https://github.com/nvim-lualine/lualine.nvim
-	local function process_diff(data)
-		local added, removed, modified = 0, 0, 0
-		for _, line in ipairs(data) do
-			if string.find(line, [[^@@ ]]) then
-				local tokens = vim.fn.matchlist(line, [[^@@ -\v(\d+),?(\d*) \+(\d+),?(\d*)]])
-				local line_stats = {
-					mod_count = tokens[3] == nil and 0 or tokens[3] == '' and 1 or tonumber(tokens[3]),
-					new_count = tokens[5] == nil and 0 or tokens[5] == '' and 1 or tonumber(tokens[5]),
-				}
-
-				if line_stats.mod_count == 0 and line_stats.new_count > 0 then
-					added = added + line_stats.new_count
-				elseif line_stats.mod_count > 0 and line_stats.new_count == 0 then
-					removed = removed + line_stats.mod_count
-				else
-					local min = math.min(line_stats.mod_count, line_stats.new_count)
-					modified = modified + min
-					added = added + line_stats.new_count - min
-					removed = removed + line_stats.mod_count - min
-				end
-			end
-		end
-		return { added = added, modified = modified, removed = removed }
-	end
-
-	local function split_lines(str)
-		local lines = {}
-		for s in str:gmatch("[^\r\n]+") do
-			table.insert(lines, s)
-		end
-		return lines
-	end
-
-	-- no opened buffer
-	if #vim.fn.expand("%") == 0 then return "" end
+	if #vim.fn.expand("%") == 0 then return "" end -- no opened buffer
 
 	local raw_diff = vim.fn.system(string.format("git --no-pager diff --no-color --no-ext-diff -U0 -- %s",
 		vim.fn.expand("%")))
 
-	-- no diff stats
-	if #raw_diff == 0 then return "" end
+	if #raw_diff == 0 then return "" end -- no diff stats
 
 	local diff_stats = process_diff(split_lines(raw_diff))
-
 	return string.format("[+%s,-%s,~%s]", diff_stats.added, diff_stats.removed, diff_stats.modified)
 end
 
