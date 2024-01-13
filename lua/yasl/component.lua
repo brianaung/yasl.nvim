3754fd7a8a3db67cc7b56afd983afe0b4f1528d2
Brian Aung <brianaung16@gmail.com>
2024-01-13 20:41:11 +1100

    refactor: improve readibility

--- lua/yasl/component.lua
+++ lua/yasl/component.lua
@@ -13,7 +13,6 @@ function M.diagnostics()
 	local ret = ""
 	local clients = vim.lsp.get_active_clients { buffer = 0 }
 	if #clients == 0 or (#clients == 1 and clients[1].name == "copilot") then
-		-- ret = "LSP: off"
 		ret = ""
 	else
 		local errors = string.format("E%s", #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }))
--- lua/yasl/init.lua
+++ lua/yasl/init.lua
@@ -1,5 +1,28 @@
-local diagnostics = require("yasl.component").diagnostics
+_G.diagnostics = require("yasl.component").diagnostics
 local branch = require("yasl.component").branch
+
+local components = {
+	["mode"] = "%{mode()}",
+	["filename"] = "%<%t%h%m%r%w",
+	["branch"] = branch(),                          -- i dont need dynamic updates
+	["diagnostics"] = "%{luaeval('diagnostics()')}", -- but here i do
+	["filetype"] = "%y",
+	["progress"] = "%P",
+	["location"] = "%-8.(%l, %c%V%)",
+}
+
+local default_sections = {
+	a = { components = { "mode" } },
+	b = { components = { "diagnostics" } },
+	c = { components = { "filename", "branch" } },
+	d = { components = { "filetype" } },
+	e = { components = { "location", "progress" } },
+}
+
+local default_refresh_events = { "LspAttach", "WinEnter", "BufEnter" }
+
+local fallback_color = vim.api.nvim_get_hl(0, { name = "StatusLine" })
+
 
 -- some component can have empty content, in which case we don't want to show the bg
 local is_component_empty = function(component)
@@ -12,46 +35,24 @@ local is_component_empty = function(component)
 	return false
 end
 
-local fallback_color = vim.api.nvim_get_hl(0, { name = "StatusLine" })
-
-local get_status_grp = function(section, hl_name)
+local get_status_grp = function(section, grp_name)
 	if section.components == nil or
 			#section.components == 0 or
 			(#section.components == 1 and is_component_empty(section.components[1])) then
 		return ""
 	end
 
-	local components = {
-		["mode"] = "%{mode()}",
-		["filename"] = "%<%t%h%m%r%w",
-		["branch"] = branch(),
-		-- ["diagnostics"] = "%{luaeval('diagnostics()')}",
-		["diagnostics"] = diagnostics(),
-		["filetype"] = "%y",
-		["progress"] = "%P",
-		["location"] = "%-8.(%l, %c%V%)",
-	}
-
 	-- set highlight (or its fallback)
-	vim.api.nvim_set_hl(0, hl_name,
-		vim.F.if_nil(section.highlight, fallback_color))
+	vim.api.nvim_set_hl(0, grp_name, vim.F.if_nil(section.highlight, fallback_color))
 
-	local curr = string.format("%s%s%s", "%#", hl_name, "#")
+	local curr = string.format("%s%s%s", "%#", grp_name, "#")
 	for i = 1, #section.components do
 		curr = string.format("%s %s", curr, components[section.components[i]])
 	end
 	return string.format("%s %s", curr, "%*")
 end
 
-local get_status_str = function(opts)
-	local default_sections = {
-		a = { components = { "mode" } },
-		b = { components = { "diagnostics" } },
-		c = { components = { "filename", "branch" } },
-		d = { components = { "filetype" } },
-		e = { components = { "location", "progress" } },
-	}
-
+local get_full_status_str = function(opts)
 	local sections = (opts and opts.sections) and opts.sections or default_sections
 
 	return table.concat({
@@ -65,16 +66,14 @@ local get_status_str = function(opts)
 	})
 end
 
-local default_refresh_events = { "LspAttach", "WinEnter", "BufEnter" }
-
 local M = {}
 
 M.setup = function(opts)
 	vim.api.nvim_set_option("laststatus", 3)
-	vim.api.nvim_set_option("statusline", get_status_str(opts))
+	vim.api.nvim_set_option("statusline", get_full_status_str(opts))
 	vim.api.nvim_create_autocmd(default_refresh_events, {
 		callback = function()
-			vim.api.nvim_set_option("statusline", get_status_str(opts))
+			vim.api.nvim_set_option("statusline", get_full_status_str(opts))
 		end
 	})
 end
