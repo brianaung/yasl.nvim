0aa0c8f7cc43e11c6d5e79891defd91921fad3fb
Brian Aung <brianaung16@gmail.com>
2024-01-15 00:46:08 +1100

    chore: manage highlight group names

--- lua/yasl/highlights.lua
+++ lua/yasl/highlights.lua
@@ -2,7 +2,8 @@ local fallback_color = vim.api.nvim_get_hl(0, { name = "StatusLine" })
 
 local M = {}
 
-function M.set_hl(name, highlight)
+function M.set_group_hl(grp, highlight)
+	local name = "YaslGroup" .. string.upper(grp)
 	vim.api.nvim_set_hl(0, name, vim.F.if_nil(highlight, fallback_color))
 end
 
