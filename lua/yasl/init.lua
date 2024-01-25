aa3e67902b8f9f3285b46bd7579b2dadfb75b35d
Brian Aung <brianaung16@gmail.com>
2024-01-25 21:08:34 +1100

    refactor: move out helpers from setup

--- lua/yasl/init.lua
+++ lua/yasl/init.lua
@@ -1,6 +1,23 @@
 local builtin_components = require("yasl.builtin_components")
 local default = require("yasl.default")
 
+local status_strings = {}
+
+local function set_statusline(str)
+	vim.api.nvim_win_set_option(0, "statusline", table.concat(str))
+end
+
+local function create_update_group(key, component)
+	local update = component.update
+	status_strings[key] = update()
+	vim.api.nvim_create_autocmd(component.events, {
+		callback = function()
+			status_strings[key] = update()
+			set_statusline(status_strings)
+		end
+	})
+end
+
 local M = {}
 
 function M.setup(opts)
@@ -11,23 +28,7 @@ function M.setup(opts)
 	vim.api.nvim_set_option("laststatus", global and 3 or 2)
 
 	-- opts.components
-	local status_strs = {}
 	local components = vim.F.if_nil(opts.components, default.components)
-
-	local function set_statusline(str)
-		vim.api.nvim_win_set_option(0, "statusline", table.concat(str))
-	end
-
-	local function create_update_group(key, component)
-		local update = component.update
-		status_strs[key] = update()
-		vim.api.nvim_create_autocmd(component.events, {
-			callback = function()
-				status_strs[key] = update()
-				set_statusline(status_strs)
-			end
-		})
-	end
 
 	for idx, component in ipairs(components) do
 		if type(component) == "table" then
@@ -35,11 +36,11 @@ function M.setup(opts)
 		elseif type(component) == "string" and builtin_components[component] ~= nil then
 			create_update_group(idx, builtin_components[component])
 		elseif type(component) == "string" then
-			status_strs[idx] = component
+			status_strings[idx] = component
 		end
 	end
 
-	set_statusline(status_strs)
+	set_statusline(status_strings)
 end
 
 return M
