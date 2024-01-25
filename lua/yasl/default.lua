ecdb79d565b18ad1c1a32977697205140ee19ed3
Brian Aung <brianaung16@gmail.com>
2024-01-25 22:07:20 +1100

    feat: add vim's builtin statusline components to defaults

--- lua/yasl/default.lua
+++ lua/yasl/default.lua
@@ -2,9 +2,13 @@ return {
 	global = true,
 	components = {
 		"mode",
-		"%t",
+		" ",
+		"%<%t%h%m%r%w", -- filename
 		"branch",
+		"%=",
 		"diagnostics",
 		"gitdiff",
+		"%=",
+		"%y %-8.(%l, %c%V%) %P", -- filetype, location, and progress
 	}
 }
