18ce1cf8b278207d43c001fb273dd35947f59cfe
Brian Aung <brianaung16@gmail.com>
2024-01-27 01:27:17 +1100

    fix: dont call update onload

--- lua/yasl/init.lua
+++ lua/yasl/init.lua
@@ -9,16 +9,20 @@ end
 
 local function create_update_group(key, component)
 	local update = component.update
-	status_strings[key] = update()
-	vim.api.nvim_create_autocmd(component.events, {
-		callback = function()
-			local new = update()
-			if status_strings[key] ~= new then
-				status_strings[key] = new
-				set_statusline(status_strings)
-			end
+	local events = component.events
+	local function update_status()
+		local new = update()
+		if status_strings[key] ~= new then
+			status_strings[key] = new
+			set_statusline(status_strings)
 		end
-	})
+	end
+	if events then
+		vim.api.nvim_create_autocmd(events, {
+			callback = update_status
+		})
+	end
+	-- todo: accept user events as well
 end
 
 local M = {}
@@ -29,11 +33,13 @@ function M.setup(opts)
 	-- opts.global
 	local global = vim.F.if_nil(opts.global, default.global)
 	vim.api.nvim_set_option("laststatus", global and 3 or 2)
+	vim.api.nvim_set_option("showmode", false)
 
 	-- opts.components
 	local components = vim.F.if_nil(opts.components, default.components)
 
 	for idx, component in ipairs(components) do
+		status_strings[idx] = "" -- empty onload
 		if type(component) == "table" then
 			create_update_group(idx, component)
 		elseif type(component) == "string" then
@@ -45,7 +51,6 @@ function M.setup(opts)
 			end
 		end
 	end
-	vim.api.nvim_set_option("showmode", false)
 
 	-- Default events to refresh statusline
 	vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
@@ -53,11 +58,14 @@ function M.setup(opts)
 			set_statusline(status_strings)
 		end
 	})
-	vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
-		callback = function()
-			vim.api.nvim_win_set_option(0, "statusline", "")
-		end
-	})
+
+	if not global then
+		vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+			callback = function()
+				vim.api.nvim_win_set_option(0, "statusline", "")
+			end
+		})
+	end
 end
 
 return M
