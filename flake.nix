f6c4b80d47d6fb3ac2fbdf6b6ebeb04e885a6f1c
brianaung <brianaung16@gmail.com>
2024-08-17 17:20:52 +1000

    feat(nix): add dev shell flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment with Lua and related tools.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil
+            lua-language-server
+            stylua
+          ];
+        };
+      });
+
+    };
+}
