02e65252e077a47276ff9f46f83bcbcbc410a1c8
brianaung <brianaung16@gmail.com>
2024-08-17 17:28:23 +1000

    refactor: reusable _set_statusline function

--- lua/yasl.lua
+++ lua/yasl.lua
@@ -1,6 +1,7 @@
+-- test
 local M = {}
 
-M._status_strings = {}
+M._status_strlist = {}
 
 M.opts = {
 	separator = " ",
@@ -23,6 +24,7 @@ M.setup = function(opts)
 
 	local group = vim.api.nvim_create_augroup("YaslSubscriptions", { clear = true })
 
+	-- A component can be either a string, or a table that defines a 'update' function
 	for idx, component in ipairs(M.opts.components) do
 		if type(component) == "table" then
 			if type(component.update) ~= "function" then
@@ -30,27 +32,40 @@ M.setup = function(opts)
 				return
 			end
 
+			-- Setup refresh events
 			local events = component.events or {}
 			if next(events) then
 				vim.api.nvim_create_autocmd(events, {
 					group = group,
 					callback = function()
 						local new = component.update() or ""
-						if M._status_strings[idx] ~= new then
-							M._status_strings[idx] = new
-							vim.opt.statusline = table.concat(M._status_strings, M.opts.separator)
+						if M._status_strlist[idx] ~= new then
+							M._status_strlist[idx] = new
+							M._set_statusline(M._status_strlist, M.opts.separator)
 						end
 					end,
 				})
 			end
 
-			table.insert(M._status_strings, component.update() or "")
+			table.insert(M._status_strlist, component.update() or "")
 		elseif type(component) == "string" then
-			table.insert(M._status_strings, component)
+			table.insert(M._status_strlist, component)
 		end
 	end
 
-	vim.opt.statusline = table.concat(M._status_strings, M.opts.separator)
+	M._set_statusline(M._status_strlist, M.opts.separator)
+end
+
+---@param status_strlist string[] An array of statusline string values evaluated
+---@param separator string Character(s) to render between sections
+M._set_statusline = function(status_strlist, separator)
+	local filtered = {}
+	for _, str in ipairs(status_strlist) do
+		if type(str) == "string" and str ~= "" then
+			table.insert(filtered, str)
+		end
+	end
+	vim.opt.statusline = table.concat(filtered, separator)
 end
 
 return M
